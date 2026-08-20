3b4b0950c7cd32a50290e903044f29a51ca67b75
Uyi Ighodaro <osaighodaro04@gmail.com>
2026-08-20 19:39:30 +0100

    Update files
    Introduce modifyData which may help with formatting some data that I have in future

--- book.cpp
+++ book.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string>
 
 #include <map>
-#include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <fstream>
@@ -19,13 +20,18 @@ struct Order {
 };
 
 // We create a map (ordered!) which uses prices as keys and vectors holding information about each 
-std::map<std::string, std::vector<Order>> buy;
-std::map<std::string, std::vector<Order>> sell;
+std::map<int, std::vector<Order>> buy;
+std::map<int, std::vector<Order>> sell;     
+
+// Then create a map (unordered!) which uses orderIDs as keys to quickly find which price point at which an order is contained in order to wipe it from the Orders vector
+std::unordered_map<int, int> ID_price_book;
 
 // If 1, then successful, if -1 then something failed
 int insertOrder(std::string direction, int price, int orderID, int size, int time) {
-    
-    return -1;
+    Order newOrder{orderID, time, size}; 
+    buy[price].push_back(newOrder);
+    ID_price_book[orderID] = price;
+    return 1;
 }
 
 // If 1, then successful, if -1 then something failed
@@ -33,16 +39,21 @@ int insertOrder(std::string direction, int price, int orderID, int size, int tim
 // I will decide if this is correct or not later when I 
 int cancelOrder(std::string direction, int price, int orderID, int size) {
     
-    return -1;
+    return 1;
 }
 
+int executeOrder() {
+    
+}
 
 int main() {
     // Not sure what a "good" method for filereading is, so I'm just gonna do "a" method
     // Initially, I left getting and parsing a message in an entirely different function, but using it in main allows me to complete necessary conversions immediately and reduces GOTO overhead
 
     // Get the CSV filename and then a variable 'line' which will hold each line of the file - I can already see how this'll scale to multithreading
-    std::ifstream csv_file("AAPL_2012-06-21_34200000_57600000_orderbook_5.csv");
+    std::string file = "testData.csv";
+    std::ifstream csv_file(file);
+    std::cout << "Dataset loaded from: " << file;
     std::string line;
     
     // Read message rows and begin parsing + working 
@@ -61,15 +72,21 @@ int main() {
         // Most important columns in order: type -> direction -> price/orderID (Insert or Cancel) -> size (for partial deletion) -> time (most relevant during executions, which are comparatively rare)
         switch(type[0]) {                                               // Strings are basically lists/vectors. Hence it's type[0] which is jarring
             case '1':                                                   // New Limit Order
-                insertOrder(direction, price, orderID, size, time);
+                if(!insertOrder(direction, price, orderID, size, time)) {
+                    std::cerr << "Error inserting order " << orderID  << std::endl;
+                }
                 break;
             case '2':                                                   // Cancel Limit Orders (Partial Deletion)
-                cancelOrder(direction, price, orderID, size);
+                if(!cancelOrder(direction, price, orderID, size)) {
+                    std::cerr << "Error canceling order " << orderID  << std::endl;
+                }
                 break;
             case '3':                                                   // Cancel Limit Orders (Total Deletion) - There is a distinction between this and 2, I'll find out what it is soon
-                cancelOrder(direction, price, orderID, size);
+                if(!cancelOrder(direction, price, orderID, size)) {
+                    std::cerr << "Error canceling order " << orderID << std::endl;
+                }
                 break;
-            case '4':                                                   // Execute visible Orders
+            case '4':                                                   // Execute visible Orders - Effectively a market buy
                 break;
             case '5':                                                   // Execute hidden Orders - This doesn't actually do anything w.r.t our book, hidden orders are ones we don't know about
                 break;
